2277a66190cb722f50b5be22dfcc33623d119b5b
Alexis Navarro <alexisnavarro@gmx.com>
2021-01-07 13:27:33 +0100

    Load authorization URL

--- app/src/main/java/com/alexis/myanimecompanion/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/alexis/myanimecompanion/ui/login/LoginViewModel.kt
@@ -1,8 +1,18 @@
 package com.alexis.myanimecompanion.ui.login
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.alexis.myanimecompanion.data.AnimeRepository
 
-class LoginViewModel {
+class LoginViewModel(animeRepository: AnimeRepository) : ViewModel() {
 
-    lateinit var webViewUrl: LiveData<String>
+    private val _webViewUrl = MutableLiveData<String>()
+    val webViewUrl: LiveData<String>
+        get() = _webViewUrl
+
+    init {
+        _webViewUrl.value = animeRepository.getAuthorizationUrl()
+    }
 }
