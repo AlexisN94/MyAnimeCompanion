a289aa3048b4265a8faee8f4783a8d3fee9ea048
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 06:54:05 +0000

    Add some of the necessary API requests

--- app/src/main/java/com/alexis/myanimecompanion/data/remote/APIClient.kt
+++ app/src/main/java/com/alexis/myanimecompanion/data/remote/APIClient.kt
@@ -1,18 +1,34 @@
 package com.alexis.myanimecompanion.data.remote
 
+import com.alexis.myanimecompanion.data.RemoteDataSource
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object APIClient {
+    const val MAL_CLIENT_ID = "cefa7feeaea5653f9eca4cd9c860ecca"
+
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request: Request = chain.request()
+                .newBuilder()
+                .addHeader("X-MAL-CLIENT-ID", MAL_CLIENT_ID)
+                .build()
+            chain.proceed(request)
+        }
         .build()
 
     val myAnimeListApi: MyAnimeListAPI = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(MyAnimeListAPI.BASE_URL)
+        .client(httpClient)
         .build()
         .create(MyAnimeListAPI::class.java)
 }
--- app/src/main/java/com/alexis/myanimecompanion/data/remote/MyAnimeListAPI.kt
+++ app/src/main/java/com/alexis/myanimecompanion/data/remote/MyAnimeListAPI.kt
@@ -1,44 +1,66 @@
 package com.alexis.myanimecompanion.data.remote
 
-import com.alexis.myanimecompanion.data.remote.models.RemoteAnime
-import com.alexis.myanimecompanion.data.remote.models.SearchResult
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import com.alexis.myanimecompanion.QueryFieldsBuilder
+import com.alexis.myanimecompanion.data.remote.models.*
+import retrofit2.http.*
 
 interface MyAnimeListAPI {
-    @GET("/anime")
+    @GET("anime")
     suspend fun search(
-        @Query("q")
-        query: String,
-
-        @Query("limit")
-        limit: Int = 100,
-
-        @Query("offset")
-        offset: Int = 0,
-
-        @Query("fields")
-        fields: String = ""
+        @Query("q") query: String? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("offset") offset: Int? = null,
+        @Query("fields") fields: String? = null,
     ): SearchResult
 
-    @GET("/anime/{anime_id}")
-    suspend fun getAnimeById(
-        @Path("anime_id")
-        id: Int,
+    @GET("anime/{anime_id}")
+    suspend fun getAnimeDetails(
+        @Path("anime_id") animeId: Int,
+        @Query("fields") fields: String? = QueryFieldsBuilder.fieldsForAnimeDetails().done(),
+        @Header("Authentication") token: String? = null,
+    ): Details
 
-        @Query("fields")
-        fields: String = ""
-    ): RemoteAnime
+    @GET("users/{user_name}/animelist")
+    suspend fun getUserAnimeList(
+        @Path("user_name") username: String
+    ): UserAnimeList
 
-    @GET("/users/{user_name}/animelist")
-    suspend fun getUserList(
-        @Path("user_name")
-        username: String
-    ) /*TODO*/
+    @GET("users/@me")
+    suspend fun getUserProfile(
+        @Header("Authorization") accessToken: String
+    ): User
+
+    @DELETE("anime/{anime_id}/my_list_status")
+    suspend fun deleteAnime(
+        @Header("Authorization") accessToken: String,
+        @Path("anime_id") animeId: Int,
+    )
+
+    // TODO PATCH or PUT ? documentation ambiguous
+    @PATCH("anime/{anime_id}/my_list_status")
+    suspend fun updateAnimeStatus(
+        @Header("Authorization") accessToken: String,
+        @Path("anime_id") animeId: Int,
+        @Field("status") status: String? = null,
+        @Field("num_watched_episodes") numWatchedEpisodes: Int? = null,
+        @Field("score") score: Int? = null
+    ): MyListStatus
+
+    @FormUrlEncoded
+    @POST(BASE_TOKEN_URL)
+    suspend fun getAccessToken(
+        @FieldMap params: Map<String, String>
+    ): Token
+
+    @FormUrlEncoded
+    @POST(BASE_TOKEN_URL)
+    suspend fun refreshAccessToken(
+        @FieldMap params: Map<String, String>
+    ): Token
 
     companion object {
-        const val CLIENT_ID = "cefa7feeaea5653f9eca4cd9c860ecca"
-        const val BASE_URL = "https://api.myanimelist.net/v2"
+        const val BASE_AUTHORIZATION_URL = "https://myanimelist.net/v1/oauth2/authorize"
+        const val BASE_TOKEN_URL = "https://myanimelist.net/v1/oauth2/token"
+        const val BASE_URL = "https://api.myanimelist.net/v2/"
     }
 }
