4132b336f8534e6a903ff4698cfc115e365a977e
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 06:38:49 +0000

    Update domain model(s)

--- app/src/main/java/com/alexis/myanimecompanion/domain/Anime.kt
+++ app/src/main/java/com/alexis/myanimecompanion/domain/Anime.kt
@@ -1,6 +1,7 @@
 package com.alexis.myanimecompanion.domain
 
 import android.os.Parcelable
+import com.alexis.myanimecompanion.data.remote.models.AlternativeTitles
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
@@ -8,11 +9,16 @@ import java.util.*
 data class Anime(
     val id: Int,
     val title: String,
-    val description: String,
     val imageUrl: String,
-    val genre: String,
-    val releaseDate: Date,
-    val globalScore: Double,
-    val releasedEpisodes: Int,
-    val status: AnimeStatus? = null
+    val synopsis: String? = null,
+    val genres: String? = null,
+    val releaseDate: Date? = null,
+    val globalScore: Double? = null,
+    val numEpisodes: Int? = null,
+    var episodesWatched: Int = 0,
+    var userScore: Int? = null,
+    var userStatus: String? = null,
+    val updatedAt: Date? = null,
+    val status: String? = null,
+    val alternativeTitles: String? = null,
 ) : Parcelable
