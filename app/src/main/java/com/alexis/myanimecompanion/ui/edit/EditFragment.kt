3437ddacd1887ed2d10c327e156742e37f24fd8b
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 06:35:14 +0000

    Add Edit view as BottomSheetDialog

--- app/src/main/java/com/alexis/myanimecompanion/ui/edit/EditFragment.kt
+++ app/src/main/java/com/alexis/myanimecompanion/ui/edit/EditFragment.kt
@@ -4,24 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
+import android.widget.ArrayAdapter
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.alexis.myanimecompanion.R
-import com.alexis.myanimecompanion.databinding.FragmentMyListBinding
+import com.alexis.myanimecompanion.data.AnimeRepository
+import com.alexis.myanimecompanion.databinding.FragmentEditBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class EditFragment : Fragment() {
+private const val MAX_RATING = 10
+private const val TAG = "EditFragment"
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding: FragmentMyListBinding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_edit,
-            container,
-            false
-        )
+class EditFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentEditBinding.inflate(inflater)
+        val args: EditFragmentArgs by navArgs()
+        val animeRepository = AnimeRepository.getInstance(requireNotNull(context))
+        val viewModelFactory = EditViewModelFactory(args.anime, animeRepository)
+        val viewModel = ViewModelProvider(viewModelStore, viewModelFactory)[EditViewModel::class.java]
 
-        binding.lifecycleOwner = viewLifecycleOwner
-        // binding.viewModel = viewModel
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            this.viewModel = viewModel
 
+            spnEditDialogStatus.adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                resources.getStringArray(R.array.anime_status)
+            )
+            spnEditDialogEpWatched.adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                IntRange(0, viewModel.anime?.numEpisodes ?: 0).toList()
+            )
+            spnEditDialogRating.adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                IntRange(0, MAX_RATING).toList()
+            )
+            btnEditDialogSave.setOnClickListener {
+                // TODO save data
+                viewModel.onSave()
+                dialog?.dismiss()
+            }
+            btnEditDialogClose.setOnClickListener {
+                // TODO dismiss only when viewModel fires dismiss event?
+                dialog?.cancel()
+            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/alexis/myanimecompanion/ui/edit/EditViewModel.kt
+++ app/src/main/java/com/alexis/myanimecompanion/ui/edit/EditViewModel.kt
@@ -1,6 +1,41 @@
 package com.alexis.myanimecompanion.ui.edit
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alexis.myanimecompanion.data.AnimeRepository
+import com.alexis.myanimecompanion.domain.Anime
+import kotlinx.coroutines.launch
 
-class EditViewModel : ViewModel() {
+private const val TAG = "EditViewModel"
+
+class EditViewModel(var anime: Anime?, private val animeRepository: AnimeRepository) : ViewModel() {
+
+    // Must be public for two-way data binding
+    val currentStatus = MutableLiveData<String?>()
+    val userScore = MutableLiveData<Int?>()
+    val episodesWatched = MutableLiveData<Int?>()
+
+    init {
+        viewModelScope.launch {
+            anime = animeRepository.getAnime(anime)
+            anime?.let {
+                episodesWatched.value = it.episodesWatched
+                currentStatus.value = it.userStatus
+                userScore.value = it.userScore
+            }
+        }
+    }
+
+    fun onSave() {
+        viewModelScope.launch {
+            anime?.let {
+                it.userStatus = currentStatus.value
+                it.userScore = userScore.value
+                it.episodesWatched = episodesWatched.value ?: 0
+                // TODO handle failure to update
+                animeRepository.updateAnimeStatus(anime)
+            }
+        }
+    }
 }
