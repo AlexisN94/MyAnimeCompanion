950542ca38c8c750fb228c746faf3ec11bc5fdd0
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 07:02:20 +0000

    Make LocalDataSource singleton and replace individual DAOs with instance of AnimeDatabase

--- app/src/main/java/com/alexis/myanimecompanion/data/LocalDataSource.kt
+++ app/src/main/java/com/alexis/myanimecompanion/data/LocalDataSource.kt
@@ -2,22 +2,20 @@ package com.alexis.myanimecompanion.data
 
 import android.content.Context
 import com.alexis.myanimecompanion.data.local.AnimeDatabase
-import com.alexis.myanimecompanion.data.local.daos.AnimeDao
-import com.alexis.myanimecompanion.data.local.daos.AnimeStatusDao
-import com.alexis.myanimecompanion.data.local.daos.UserDao
-import com.alexis.myanimecompanion.domain.Anime
 
-class LocalDataSource(private val context: Context) {
-    private lateinit var animeDao: AnimeDao
-    private lateinit var userDao: UserDao
-    private lateinit var animeStatusDao: AnimeStatusDao
+class LocalDataSource private constructor() {
+    private lateinit var animeDatabase: AnimeDatabase
 
-    init {
-        val animeDatabase = AnimeDatabase.getInstance(context)
-        animeDatabase.let {
-            animeDao = it.animeDao
-            userDao = it.userDao
-            animeStatusDao = it.animeStatusDao
+    companion object {
+        private var INSTANCE: LocalDataSource? = null
+
+        fun getInstance(context: Context): LocalDataSource {
+            synchronized(this) {
+                return INSTANCE ?: LocalDataSource().also { instance ->
+                    instance.animeDatabase = AnimeDatabase.getInstance(context)
+                    INSTANCE = instance
+                }
+            }
         }
     }
 }
