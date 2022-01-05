2891d8378670bea2eb25a2d7bdf7abb044188bbf
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 06:08:38 +0000

    Add bottom navigation view

--- app/src/main/java/com/alexis/myanimecompanion/ui/MainActivity.kt
+++ app/src/main/java/com/alexis/myanimecompanion/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.alexis.myanimecompanion.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.alexis.myanimecompanion.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
     }
 }
