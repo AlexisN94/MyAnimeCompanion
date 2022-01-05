d3558680fcd9bbd0f84a6bbdec85704d3fd01d14
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 06:19:25 +0000

    Add LoginFragment and view model

--- app/src/main/java/com/alexis/myanimecompanion/ui/MainActivity.kt
+++ app/src/main/java/com/alexis/myanimecompanion/ui/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.alexis.myanimecompanion.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.alexis.myanimecompanion.R
+import com.alexis.myanimecompanion.data.RemoteDataSource
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +21,25 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val data = intent?.data
+        val scheme = data?.scheme
+        val host = data?.host
+
+        if (scheme == "oauth" && host == "callback") {
+            handleMALAuthorizationResponse(data)
+        }
+    }
+
+    private fun handleMALAuthorizationResponse(data: Uri) {
+        val authorizationCode = requireNotNull(data.getQueryParameter("code"))
+        lifecycleScope.launch {
+            val a = RemoteDataSource.getInstance()
+            a.getAccessToken(authorizationCode)
+        }
     }
 }
