d101b4d67706b99602e91d1502de145ce20e786b
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-05 06:59:12 +0000

    Change AnimeRepository arguments to require context only & Add basic methods

--- app/src/main/java/com/alexis/myanimecompanion/data/AnimeRepository.kt
+++ app/src/main/java/com/alexis/myanimecompanion/data/AnimeRepository.kt
@@ -1,19 +1,41 @@
 package com.alexis.myanimecompanion.data
 
-class AnimeRepository private constructor(
-    private val localDataSource: LocalDataSource,
-    private val remoteDataSource: RemoteDataSource
-) {
+import android.content.Context
+import com.alexis.myanimecompanion.domain.Anime
+
+class AnimeRepository private constructor() {
+
+    suspend fun search(q: String): List<Anime>? {
+        return remoteDataSource.search(q)
+    }
+
+    suspend fun updateAnimeStatus(anime: Anime?) {
+        /* TODO Save changes to localDataSource and remoteDataSource if applicable */
+    }
+
+    suspend fun getAnime(anime: Anime?): Anime? {
+        /* TODO return complete? Anime object with fresh user-specific status */
+        return remoteDataSource.getAnimeDetails(anime)
+    }
+
+    suspend fun getAnime(animeId: Int): Anime? {
+        /* TODO return complete? Anime object with fresh user-specific status */
+        return remoteDataSource.getAnimeDetails(animeId)
+    }
 
     companion object {
         private var INSTANCE: AnimeRepository? = null
+        private lateinit var localDataSource: LocalDataSource
+        private lateinit var remoteDataSource: RemoteDataSource
 
-        fun getInstance(localDataSource: LocalDataSource, remoteDataSource: RemoteDataSource): AnimeRepository {
+        fun getInstance(context: Context): AnimeRepository {
             synchronized(this) {
                 return INSTANCE
-                    ?: AnimeRepository(localDataSource, remoteDataSource)
+                    ?: AnimeRepository()
                         .also { animeRepo ->
                             INSTANCE = animeRepo
+                            localDataSource = LocalDataSource.getInstance(context)
+                            remoteDataSource = RemoteDataSource.getInstance()
                         }
             }
         }
