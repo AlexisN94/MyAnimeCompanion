559408fe878e2eb00f6d5793af5ff7910eb76fa7
Alexis Navarro <alexisnavarro@gmx.com>
2022-01-10 06:57:02 +0000

    Create utility class to manage token storage

--- app/src/main/java/com/alexis/myanimecompanion/data/remote/models/Token.kt
+++ app/src/main/java/com/alexis/myanimecompanion/data/remote/models/Token.kt
@@ -1,8 +1,20 @@
 package com.alexis.myanimecompanion.data.remote.models
 
+import com.alexis.myanimecompanion.domain.DomainToken
+
 data class Token(
-    val access_token: String = "",
-    val expires_in: Int = 0,
-    val refresh_token: String = "",
-    val token_type: String = ""
+    var access_token: String,
+    var expires_in: Int,
+    var refresh_token: String,
+    val token_type: String
 )
+
+fun Token.asDomainModel(): DomainToken {
+    val expiresAt = System.currentTimeMillis() + expires_in
+    return DomainToken(
+        access_token,
+        expiresAt,
+        refresh_token,
+        token_type
+    )
+}
